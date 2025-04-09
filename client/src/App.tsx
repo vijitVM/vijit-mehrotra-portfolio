5372101539502272aa2f88aefd62db0af93fdc12
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-04-09 18:17:36 +0530

    App File has be improved for Skills Section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -87,7 +87,7 @@ function App() {
           if (hash === 'skills') {
             // This ensures the radar chart is visible, similar to the second image
             // We add an offset to scroll further down to show the chart
-            elementPosition = elementPosition + 200;
+            elementPosition = elementPosition + 100;
           }
           
           const offsetPosition = elementPosition - headerHeight - 10;

0bed23feba59ce2dc43995484daf792c87bf16e9
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-04-10 12:43:04 +0530

    updated the asset data

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
       
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@asset": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
   root: path.resolve(import.meta.dirname, "client"),

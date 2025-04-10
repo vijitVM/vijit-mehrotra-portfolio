2d5c47bac65aeaede371238dfd5710e8f97c9f81
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-04-10 13:43:58 +0530

    data fixed to make logo appear

--- client/src/data/data.ts
+++ client/src/data/data.ts
@@ -6,8 +6,9 @@ import CNNFilterImage from "../attached_assets/block1_conv1_filter27.png";
 import ChatPDFImage from "../attached_assets/chat_with_PDF.png";
 import FirstAmericanLogo from "../attached_assets/first_american_india_logo.jpeg";
 import ConvergyticsLogo from "../attached_assets/convergytics_solutions_logo.jpeg";
-import clevrlogo from "../attached_assets/clevr.png";
-import quationlogo from "../attached_assets/quation.jpeg";
+import clevrlogo from "@asset/clevr.png";
+import quationlogo from "@asset/quation.jpeg";
+
 
 export const skillsData = {
   coreSkills: [
@@ -53,7 +54,7 @@ export const experienceData = [
     color: "bg-blueAccent",
     textColor: "text-blueAccent",
     logo: quationlogo,
-    logoType: "text",
+    logoType: "image",
     positions: [
       {
         id: 101,
@@ -179,7 +180,7 @@ export const experienceData = [
     color: "bg-blueAccent",
     textColor: "text-blueAccent",
     logo: clevrlogo,
-    logoType: "text",
+    logoType: "image",
     positions: [
       {
         id: 301,

f34f86dfdee0c8e1e23ec24446ab81a80d3ff1ce
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-05-10 17:26:40 +0530

    Changed again border for project section

--- client/src/components/ProjectsSection.tsx
+++ client/src/components/ProjectsSection.tsx
@@ -99,8 +99,8 @@ const ProjectsSection = () => {
 
 
   return (
-    <section id="projects" className="py-0.5 pt-0.5 bg-gray-900/50" ref={sectionRef}>
-      <div className="sm:w-full px-0 sm:px-1 lg:w-11/12 xl:w-5/6 py-1 border-b-gray-800">
+    <section id="projects" className="py-1 pt-1 bg-gray-900/50" ref={sectionRef}>
+      <div className="sm:w-full px-0 sm:px-2 lg:w-11/12 xl:w-5/6 py-3 border-b-gray-800">
         <motion.div
           variants={headerVariants}
           initial="hidden"

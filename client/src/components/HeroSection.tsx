4495224efaa5f3e00d3087f91b128f82ad60fb72
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-05-11 06:46:02 +0530

    Increased the Height for proper fitting

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -221,7 +221,7 @@ const HeroSection = () => {
             </motion.div>
 
             {/* Right column (text content) */}
-            <div className="flex items-center justify-start w-full md:w-[60%] pb-6">
+            <div className="flex items-center justify-start w-full md:w-6.5/12 pb-6">
               <div className="items-start justify-center w-full md:min-h-[60vh] flex flex-col gap-4">
                 <motion.div
                   className="flex flex-col gap-2"

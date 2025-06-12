b07e150b9b911004b634e48f3c3497c0dacc3eae
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-06-12 21:05:47 +0530

    Updated tailwind.config.ts with other plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,5 +86,10 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  // ADD THIS LINE
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require('tailwind-scrollbar-hide') // <--- ADD THIS PLUGIN
+  ],
 } satisfies Config;

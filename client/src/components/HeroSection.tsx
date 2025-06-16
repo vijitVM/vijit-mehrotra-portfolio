74856e1a20a9f69e81932eed43aec1b8bbc40ed3
Vijit Mehrotra <58991592+vijitVM@users.noreply.github.com>
2025-06-16 21:18:17 +0530

    reduced the left and right margins

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -153,7 +153,7 @@ const HeroSection = () => {
     <section
       id="home"
       
-      className="flex flex-col items-center justify-center min-h-full w-full px-4 py-12 border-b-[1px] border-b-gray-800"
+      className="flex flex-col items-center justify-center min-h-full w-full px-2 py-12 border-b-[1px] border-b-gray-800"
       ref={heroRef}
       onMouseMove={handleMouseMove}
     >
